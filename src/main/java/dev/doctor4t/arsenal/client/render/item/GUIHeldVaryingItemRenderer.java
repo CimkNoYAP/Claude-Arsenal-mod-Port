4cd21cd89f83eb4ed9398c0ffaa0ab0fac0ededa
CimkNo <simkiflorek@gmail.com>
2026-07-13 22:41:27 +0200

    Refactor GUIHeldVaryingItemRenderer to abstract class

--- src/main/java/dev/doctor4t/arsenal/client/render/item/GUIHeldVaryingItemRenderer.java
+++ src/main/java/dev/doctor4t/arsenal/client/render/item/GUIHeldVaryingItemRenderer.java
@@ -1,67 +1,50 @@
 package dev.doctor4t.arsenal.client.render.item;
 
-import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
-import net.fabricmc.fabric.api.resource.ResourceReloadListenerKeys;
-import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import dev.doctor4t.arsenal.client.ArsenalClient;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.item.BuiltinModelItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
-import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
-import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
+import net.minecraft.client.util.ModelIdentifier;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Set;
-
-public class GUIHeldVaryingItemRenderer implements BuiltinItemRendererRegistry.DynamicItemRenderer, SimpleSynchronousResourceReloadListener {
-    private static final Set<ModelTransformationMode> inventoryModes = Set.of(ModelTransformationMode.GUI, ModelTransformationMode.GROUND);
-    private final Identifier id;
-    private final Identifier weaponId;
-    private ItemRenderer itemRenderer;
+public abstract class GUIHeldVaryingItemRenderer extends BuiltinModelItemRenderer {
+    protected final Identifier weaponId;
     private BakedModel inventoryWeaponModel;
     private BakedModel worldWeaponModel;
 
     public GUIHeldVaryingItemRenderer(Identifier weaponId) {
-        this.id = Identifier.of(weaponId.getNamespace(), weaponId.getPath() + "_renderer");
+        super(MinecraftClient.getInstance().getBlockEntityRenderDispatcher(),
+              MinecraftClient.getInstance().getEntityModelLoader());
         this.weaponId = weaponId;
     }
 
     @Override
-    public Identifier getFabricId() {
-        return this.id;
-    }
-
-    @Override
-    public Collection<Identifier> getFabricDependencies() {
-        return Collections.singletonList(ResourceReloadListenerKeys.MODELS);
-    }
-
-    @Override
-    public void reload(ResourceManager manager) {
-        final MinecraftClient client = MinecraftClient.getInstance();
-        this.itemRenderer = client.getItemRenderer();
-        this.inventoryWeaponModel = client.getBakedModelManager().getModel(new ModelIdentifier(Identifier.of(this.weaponId.getNamespace(), this.weaponId.getPath() + "_gui"), "inventory"));
-        this.worldWeaponModel = client.getBakedModelManager().getModel(new ModelIdentifier(Identifier.of(this.weaponId.getNamespace(), this.weaponId.getPath() + "_handheld"), "inventory"));
-    }
-
-    @Override
-    public void render(ItemStack stack, ModelTransformationMode mode, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-        matrices.pop();
-        matrices.push();
-        if (inventoryModes.contains(mode)) {
-            this.itemRenderer.renderItem(stack, mode, false, matrices, vertexConsumers, light, overlay, this.inventoryWeaponModel);
-        } else {
-            boolean leftHanded;
-            switch (mode) {
-                case FIRST_PERSON_LEFT_HAND, THIRD_PERSON_LEFT_HAND -> leftHanded = true;
-                default -> leftHanded = false;
-            }
-            this.itemRenderer.renderItem(stack, mode, leftHanded, matrices, vertexConsumers, light, overlay, this.worldWeaponModel);
+    public void render(ItemStack stack, ModelTransformationMode mode, MatrixStack matrices,
+                       VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        if (this.inventoryWeaponModel == null || this.worldWeaponModel == null) {
+            // Lazy-load models after the model manager is ready
+            var manager = MinecraftClient.getInstance().getBakedModelManager();
+            this.inventoryWeaponModel = manager.getModel(
+                new ModelIdentifier(Identifier.of(weaponId.getNamespace(), weaponId.getPath() + "_inventory"), "inventory"));
+            this.worldWeaponModel = manager.getModel(
+                new ModelIdentifier(Identifier.of(weaponId.getNamespace(), weaponId.getPath() + "_in_hand"), "inventory"));
         }
+
+        ArsenalClient.currentMode = mode;
+        BakedModel model = switch (mode) {
+            case GUI, GROUND, FIXED -> this.inventoryWeaponModel;
+            default -> this.worldWeaponModel;
+        };
+
+        renderModel(stack, mode, matrices, vertexConsumers, light, overlay, model);
+        ArsenalClient.currentMode = ModelTransformationMode.NONE;
     }
+
+    protected abstract void renderModel(ItemStack stack, ModelTransformationMode mode, MatrixStack matrices,
+                                        VertexConsumerProvider vertexConsumers, int light, int overlay,
+                                        BakedModel model);
 }
